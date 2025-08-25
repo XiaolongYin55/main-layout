8ef729ef1aa043ff246419c7c48a4f53e617bc7d
Xiaolong Yin <xiaolong@graduate.utm.my>
2025-08-25 17:49:37 +0800

    db-version

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
  const MyApp({super.key});
 
-
+// this is the new db version
  @override
  Widget build(BuildContext context) {
    return MaterialApp(
